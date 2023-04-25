6a8fa856bb4269610d5565643590218109f48da5
tom <tomkit@gmail.com>
2023-04-25 11:57:19 -0700

    chore: allow salesforce

--- index.ts
+++ index.ts
@@ -14,19 +14,19 @@ const watermarkManager = new WatermarkManager();
 const paginators: SupagluePaginator[] = [];
 
 const supportedCommonModels = [
-  'account',
-  'contact',
-  'lead',
-  'opportunity',
-  'user',
+  "account",
+  "contact",
+  "lead",
+  "opportunity",
+  "user",
 ];
 
 const commonModelToObjectListName: Record<string, string> = {
-  account: 'accounts',
-  contact: 'contacts',
-  lead: 'leads',
-  opportunity: 'opportunities',
-  user: 'users',
+  account: "accounts",
+  contact: "contacts",
+  lead: "leads",
+  opportunity: "opportunities",
+  user: "users",
 };
 
 app.post("/supaglue_sync_webhook", async (req, res) => {
@@ -42,21 +42,18 @@ app.post("/supaglue_sync_webhook", async (req, res) => {
   ) {
     return res
       .status(200)
-      .send("not a sync success event or no customer_id/provider_name/common_model");
+      .send(
+        "not a sync success event or no customer_id/provider_name/common_model"
+      );
   }
 
   const customerId = req.body.payload.customer_id;
   const providerName = req.body.payload.provider_name;
   const commonModel = req.body.payload.common_model;
-  
-  if (providerName !== 'hubspot') {
-    console.log("only hubspot is supported");
-    return res.status(200).send('only hubspot is supported');
-  }
 
   if (!supportedCommonModels.includes(commonModel)) {
-    console.log('sync event for object type not supported', commonModel);
-    return res.status(200).send('sync event for object type not supported');
+    console.log("sync event for object type not supported", commonModel);
+    return res.status(200).send("sync event for object type not supported");
   }
 
   const objectListName = commonModelToObjectListName[commonModel];
@@ -71,8 +68,8 @@ app.post("/supaglue_sync_webhook", async (req, res) => {
   });
 
   if (existingPaginator) {
-    console.log('already syncing', objectListName, customerId, providerName);
-    return res.status(200).send('already syncing');
+    console.log("already syncing", objectListName, customerId, providerName);
+    return res.status(200).send("already syncing");
   }
 
   // Start syncing this object type
@@ -83,20 +80,28 @@ app.post("/supaglue_sync_webhook", async (req, res) => {
     customerId,
     providerName,
     destination: isS3Destination
-      ? new S3Destination(objectListName, customerId, providerName, syncStartTime)
+      ? new S3Destination(
+          objectListName,
+          customerId,
+          providerName,
+          syncStartTime
+        )
       : new PrismaDestination(objectListName, syncStartTime),
     incremental: isS3Destination ? false : true,
     watermarkManager,
   });
 
   paginators.push(supagluePaginator);
-  
-  supagluePaginator.start().catch((err) => {
-    console.error('error syncing', objectListName, err);
-  }).finally(() => {
-    // Clean up when done
-    paginators.splice(paginators.indexOf(supagluePaginator), 1);
-  });
+
+  supagluePaginator
+    .start()
+    .catch((err) => {
+      console.error("error syncing", objectListName, err);
+    })
+    .finally(() => {
+      // Clean up when done
+      paginators.splice(paginators.indexOf(supagluePaginator), 1);
+    });
 
   return res.send("ok");
 });
